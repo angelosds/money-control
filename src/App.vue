078d8cdf6b14f25ae2089e692fd36f2fb121037f
Angelo Santos <angelo.santos@foster.com.br>
2018-06-11 16:46:55 -0300

    Removed material-components packages

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,5 @@
 <template>
-  <header class="mdc-top-app-bar">
-    <div class="mdc-top-app-bar__row">
-      <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
-        <a
-          href="#"
-          class="material-icons mdc-top-app-bar__navigation-icon">menu</a>
-        <span class="mdc-top-app-bar__title">Title</span>
-      </section>
-    </div>
-  </header>
+  <div>App</div>
 </template>
 
 <script>
@@ -23,5 +14,5 @@
 </script>
 
 <style lang="scss">
-  @import "@material/top-app-bar/mdc-top-app-bar";
+
 </style>
