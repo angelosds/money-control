a3572c605202451f917f9a15fad3fa5ede465bcc
Angelo Santos <angelo.santos@foster.com.br>
2018-06-26 12:25:50 -0300

    Page title from routes

--- src/pages/balance.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    Balance
-  </div>
-</template>
--- src/router.js
+++ src/router.js
@@ -11,33 +11,60 @@ import Cards from './pages/cards'
 import Account from './pages/account'
 import Settings from './pages/settings'
 
+import store from './store'
+
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    component: Home
+    component: Home,
+    meta: {
+      title: 'Home'
+    }
   },
   {
     path: '/balance',
-    component: Balance
+    component: Balance,
+    meta: {
+      title: 'Balance'
+    }
   },
   {
     path: '/cards',
-    component: Cards
+    component: Cards,
+    meta: {
+      title: 'Cards'
+    }
   },
   {
     path: '/account',
-    component: Account
+    component: Account,
+    meta: {
+      title: 'Account'
+    }
   },
   {
     path: '/settings',
-    component: Settings
+    component: Settings,
+    meta: {
+      title: 'Settings'
+    }
   }
 ]
 
-export default new VueRouter({
+const router = new VueRouter({
   mode: 'history',
   linkActiveClass: '-active',
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.hasOwnProperty('title')) {
+    store.commit('setTitle', to.meta.title)
+  }
+
+  next();
+})
+
+export default router
